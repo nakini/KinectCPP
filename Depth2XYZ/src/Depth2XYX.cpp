89d43667c4228f612fc6e16e92f6cbb94be17afc
nakini <tnb88@mail.missouri.edu>
2016-11-23 00:51:59 -0600

    Added libfreenect camera calibration info

--- Depth2XYZ/src/Depth2XYX.cpp
+++ Depth2XYZ/src/Depth2XYX.cpp
@@ -1,7 +1,23 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
 
-using namespace cv;
+//using namespace cv;
+
+//IR Camera parameters as given by libfreenect2
+struct camera_params_t
+{
+    struct ir_t{
+        float cx = 254.878f;
+        float cy = 205.395f;
+        float fx = 365.456f;
+        float fy = 365.456f;
+        float k1 = 0.0905474;
+        float k2 = -0.26819;
+        float k3 = 0.0950862;
+        float p1 = 0.0;
+        float p2 = 0.0;
+    }ir;
+}camera_params;
 
 int main(int argc, char** argv )
 {
@@ -11,18 +27,18 @@ int main(int argc, char** argv )
         return -1;
     }
 
-    Mat image;
-    image = imread( argv[1], 1 );
+    cv::Mat image;
+    image = cv::imread( argv[1], 1 );
 
     if ( !image.data )
     {
         printf("No image data \n");
         return -1;
     }
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", image);
+    cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE );
+    cv::imshow("Display Image", image);
 
-    waitKey(0);
+    cv::waitKey(0);
 
     return 0;
 }
