38563672b077773c5c768b73f4b81af68f7ca125
nakini <tnb88@mail.missouri.edu>
2016-11-23 11:37:03 -0600

    Added depth to point cloud function, but not checked yet

--- Depth2XYZ/src/Depth2XYX.cpp
+++ Depth2XYZ/src/Depth2XYX.cpp
@@ -19,6 +19,32 @@ struct camera_params_t
     }ir;
 }camera_params;
 
+bool depth_image_to_point_cloud(){
+    //Process Depth To PCL Cloud
+    uint pixel_count = depth.rows * depth.cols;
+    dst->resize(pixel_count);
+    dst->height = depth.rows;
+    dst->width = depth.cols;
+
+    float x = 0.0f, y = 0.0f;
+
+    float* ptr = (float*) (depth.data);
+    for (uint i = 0; i < pixel_count; ++i)
+    {
+        cv::Vec2f xy = xycords.at<cv::Vec2f>(0, i);
+        x = xy[1]; y = xy[0];
+
+        PointT point;
+        point.z = (static_cast<float>(*ptr)) / (1000.0f); // Convert from mm to meters
+        point.x = (x - config.camera_params.ir.cx) * point.z / config.camera_params.ir.fx;
+        point.y = (y - config.camera_params.ir.cy) * point.z / config.camera_params.ir.fy;
+        point.r = 128; point.g = 128; point.b = 128;
+        dst->at(i) = point;
+
+        ++ptr;
+    }
+}
+
 int main(int argc, char** argv )
 {
     if ( argc != 2 )
