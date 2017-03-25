bab879f5265e49e2bb5912814223f9478562eec7
nakini <tnb88@mail.missouri.edu>
2017-03-24 19:10:12 -0500

    Added some helper files

---
+++ SampleImages/utils/GetImageList.cpp
@@ -0,0 +1,7 @@
+#include "GetImageList.h"
+
+// This function creates a list of names of the image files present in the given directory.
+// The default image format is JPG
+std::vector<std::__cxx11::string> GetImageList(std::__cxx11::string dirName){
+
+}
---
+++ SampleImages/utils/GetImageList.h
@@ -0,0 +1,9 @@
+#ifndef  __GET_IMAGE_LIST__
+#define  __GET_IMAGE_LIST__
+#include <string>
+#include <vector>
+#include <iostream>
+
+std::vector<std::string> GetImageList(std::string);
+
+#endif
