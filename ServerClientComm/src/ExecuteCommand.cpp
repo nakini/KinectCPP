f2525d4c75ee1e3fa6a954f09507de48faa9d30a
nakini <tnb88@mail.missouri.edu>
2018-07-06 11:30:19 -0500

    Add source file to execute system commands from c/cpp file

---
+++ ServerClientComm/src/ExecuteCommand.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <array>
+
+std::string exec(const char* cmd) {
+    std::array<char, 128> buffer;
+    std::string result;
+    std::shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
+    if (!pipe) throw std::runtime_error("popen() failed!");
+    while (!feof(pipe.get())) {
+        if (fgets(buffer.data(), 128, pipe.get()) != nullptr)
+            result += buffer.data();
+    }
+    return result;
+}
+
+int main(){
+    std::cout << "Hello world" << std::endl;
+    std::string outputMsg =  exec("ls /home/nakini/Desktop/Test/depth* | wc");
+    std::cout << "Total number of files of each type are: " << outputMsg;
+    return 0;
+}
